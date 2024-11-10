a2437e94f9a05c94b9c3cbea83e94d814608b6a7
Adam Marciniak <adam668@gmail.com>
2024-11-09 19:53:21 -0800

    s

--- public/broadcast.js
+++ public/broadcast.js
@@ -13,7 +13,7 @@ const config = {
   ],
 };
 
-const socket = io.connect("https://courtdates.ca/webrtc");
+const socket = io.connect("https://courtdates2.ca/webrtc");
 
 socket.on("answer", (id, description) => {
   peerConnections[id].setRemoteDescription(description);
